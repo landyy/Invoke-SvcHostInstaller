6671f5178e9f1cbfbcf0745fb769802f9857d9ca
landyy <rmb2555@rit.edu>
2019-04-10 00:59:17 -0400

    Script now 100% works on Windows 10 but refuses to work on 7. Added a ton of checks and QOL improvements

--- Invoke-SvcHostInstaller.ps1
+++ Invoke-SvcHostInstaller.ps1
@@ -1,5 +1,4 @@
-﻿
-function Install-SvcHostService{
+﻿function Install-SvcHostService{
     Param(
     [Parameter( Position = 0, Mandatory = $true )]
     [String]$ServiceName,
@@ -14,22 +13,59 @@ function Install-SvcHostService{
     [String]$ServicePrimaryGroup,
 
     [Parameter( Position = 4, Mandatory = $false )]
-    [string]$Description
+    [AllowNull()]
+    [AllowEmptyString()]
+    [string]$Description = "Example of a Service Loaded via SVCHOST"
     )
+
+    if(!$PSBoundParameters.ContainsKey('Description')){
+        Write-Warning "Description is set to the default. This is NOT opsec :eyes:"
+    }
+
+    #Check for older versions of powershell since the "Requires" thing is >=4
+    $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
     
+    if(!$currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)){
+         Write-Error -Exception "Not Running as Administrator" -Message "Please run this script as an administrator."
+        Exit
+    }
+
     #Checks are below hereeeee
-    $PossibleGroups = Get-Item "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SVCHOST" | Select -ExpandProperty Property
+    $PossibleGroups = Get-Item "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SVCHOST" | Select-Object -ExpandProperty Property
 
     #First check if the request service group exsists
-    if(!$PossibleGroups.Contains($ServicePrimaryGroup)){
+    if(!$PossibleGroups -contains $ServicePrimaryGroup){
         Write-Error -Exception "Bad Service Group" -Message "Requested service group not found."
+        Exit
     }
+
+    #Check if the service already exsists
+    $ServiceCheck = Get-Service -ServiceName $ServiceName -ErrorAction "SilentlyContinue"
+
+    #get the full path and make sure it exsits
+    $FullPath = Get-Item $ServiceDll | Select-Object FullName
+    $Temp = Test-Path $FullPath.FullName
+    if(!$Temp){
+        Write-Error -Exception "File Note Found" -Message "The Requested Dll was not found."
+        Exit
+    }
+
+    $DllName = Get-Item $ServiceDll | Select-Object Name
+    $FullSystemPath = "C:\Windows\System32\" + $DllName.Name
+
+    #Move it to system32 to be nice :)
+    Copy-Item -Path $FullPath.FullName -Destination  $FullSystemPath -Force
 
     $SvcPath = "C:\Windows\System32\svchost.exe -k " + $ServicePrimaryGroup
 
+    if($ServiceCheck){
+        Write-Error -Exception "Bad Service Name" -Message "Requested service name already in use."
+        Exit
+    }
+
     #Create the service using New-Service
     #many change this later
-    New-Service -Name $ServiceName -DisplayName $DisplayName -BinaryPathName $SvcPath -StartupType Automatic
+    New-Service -Name $ServiceName -DisplayName $DisplayName -BinaryPathName $SvcPath -StartupType Automatic -Description $Description
 
     $BasePath = "HKLM:\SYSTEM\CurrentControlSet\Services\" + $ServiceName
     $RegPath = $BasePath + "\Parameters"
@@ -39,15 +75,23 @@ function Install-SvcHostService{
 
     #Create the value ServiceDLL
     #MUST BE FULL PATH
-    New-ItemProperty -Path $RegPath -PropertyType ExpandString -Name ServiceDll -Value $ServiceDll
+    New-ItemProperty -Path $RegPath -PropertyType ExpandString -Name ServiceDll -Value $FullSystemPath
 
     #Set the service type to WIN32_SHARED_PROCESS (0x20)
     Set-ItemProperty -Path $BasePath -Name Type -Value 0x20
 
-    $GroupRegLocation = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SVCHOST"
+    #https://stackoverflow.com/questions/27238523/editing-a-multistring-array-for-a-registry-value-in-powershell
+    $GroupRegLocation = "SOFTWARE\Microsoft\Windows NT\CurrentVersion\SVCHOST"
+    
+    $reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', "")
+    $key = $reg.OpenSubKey($GroupRegLocation, $true)
+    $arr = $key.GetValue($ServicePrimaryGroup)
 
-    $GroupMems = Get-ItemProperty $GroupRegLocation -Name $ServicePrimaryGroup | select -ExpandProperty $ServicePrimaryGroup
-    $GroupMems += "`n"
-    $GroupMems += $ServiceName
-    Set-ItemProperty $GroupRegLocation -Name $ServicePrimaryGroup -Value $GroupMems
+    $arr += $ServiceName
+
+    $key.SetValue($ServicePrimaryGroup, [string[]]$arr, 'MultiString')
+
+    Start-Service $ServiceName | Out-Null
+
+    Get-Service $ServiceName
 }
